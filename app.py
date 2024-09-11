82e8f0460ee639e217c2c912beba8e571b3d86bd
AratiSankaliya12 <aratisankaliya12@gmail.com>
2024-09-11 11:20:33 +0530

    Print-Hello World!

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_pymongo import PyMongo
 
+print("Hello World!")
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
